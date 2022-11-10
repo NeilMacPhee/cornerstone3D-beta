1cb152cc5b38dfdcea4814ed0cac61149dedab95
Neil <neil.a.macphee@gmail.com>
2022-11-10 14:13:45 -0500

    vtiVolumeLoader to be called by createAndCacheVolume to create volumes from vti files

--- packages/core/examples/vtiVolumeLoader/vtiVolumeLoader.ts
+++ packages/core/examples/vtiVolumeLoader/vtiVolumeLoader.ts
@@ -1,9 +1,17 @@
 import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
+import vtkXMLImageDataReader from '@kitware/vtk.js/IO/XML/XMLImageDataReader';
+
+import '@kitware/vtk.js/Rendering/Profiles/Volume';
+
+// Force DataAccessHelper to have access to various data source
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/HtmlDataAccessHelper';
 import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/JSZipDataAccessHelper';
+
 import { ImageVolume, Types } from '@cornerstonejs/core';
 
 function createImageVolume(
-  tipo: string,
+  volumeType: string,
   dimensions: Types.Point3,
   spacing: Types.Point3,
   origin: Types.Point3,
@@ -11,36 +19,52 @@ function createImageVolume(
   scalarData: any,
   volumeId = 'NEW'
 ) {
-  let metadata: Types.Metadata;
-  let volume: ImageVolume;
-  if (tipo === 'Uint8Array') {
-    metadata = {
-      BitsAllocated: 8,
-      BitsStored: 8,
-      HighBit: 7,
-      ImageOrientationPatient: [0, 0, 1, 1, 0, 0, 0, 1, 0],
-      SamplesPerPixel: 1,
-      PhotometricInterpretation: 'MONOCHROME2',
-      PixelRepresentation: 0,
-      Modality: 'SC',
-      PixelSpacing: spacing,
-      FrameOfReferenceUID: '11111',
-      Columns: dimensions[0],
-      Rows: dimensions[1],
-      voiLut: [],
-    };
-    volume = new ImageVolume({
-      volumeId,
-      metadata,
-      dimensions,
-      spacing,
-      origin,
-      direction,
-      scalarData,
-      sizeInBytes: scalarData.length,
-    });
-    return volume;
+  // let metadata: Types.Metadata;
+  // let volume: ImageVolume;
+  let hightBit = 0;
+  let bitsStored = 0;
+
+  console.log(volumeType);
+  if (volumeType.indexOf('int8') >= 0) {
+    hightBit = 7;
+    bitsStored = 8;
+  } else if (volumeType.indexOf('16') >= 0) {
+    hightBit = 15;
+    bitsStored = 16;
+  } else if (volumeType.indexOf('32') >= 0) {
+    hightBit = 31;
+    bitsStored = 32;
   }
+
+  const metadata = {
+    BitsAllocated: bitsStored,
+    BitsStored: bitsStored,
+    HighBit: hightBit,
+    ImageOrientationPatient: [0, 0, 1, 1, 0, 0, 0, 1, 0],
+    SamplesPerPixel: 1,
+    PhotometricInterpretation: 'MONOCHROME2',
+    PixelRepresentation: 0,
+    Modality: 'SC',
+    PixelSpacing: spacing,
+    FrameOfReferenceUID: '11111',
+    Columns: dimensions[0],
+    Rows: dimensions[1],
+    voiLut: [],
+  };
+  const volume = new ImageVolume({
+    volumeId,
+    metadata,
+    dimensions,
+    spacing,
+    origin,
+    direction,
+    scalarData,
+    sizeInBytes: Math.floor((scalarData.length * bitsStored) / 8),
+  });
+  console.log('Volume Opened : ');
+  console.log(volume);
+
+  return volume;
 }
 
 function vtiVolumeLoader(volumeId: string, { imageIds }) {
@@ -48,27 +72,35 @@ function vtiVolumeLoader(volumeId: string, { imageIds }) {
     promise: new Promise((resolve, reject) => {
       try {
         const tokens = imageIds[0].split(':');
+        const fetchGzip = false;
+        const xml = true;
         tokens.shift();
         const url = tokens.join(':');
-        const reader = vtkHttpDataSetReader.newInstance({ fetchGzip: true });
-        reader
-          .setUrl(url)
-          .then(() => reader.loadData())
-          .then(() => {
-            const data = reader.getOutputData();
-            const scalars = reader.getOutputData().getPointData().getScalars();
-            resolve(
-              createImageVolume(
-                scalars.getDataType() as string,
-                data.getDimensions() as Types.Point3,
-                data.getSpacing() as Types.Point3,
-                data.getOrigin() as Types.Point3,
-                data.getDirection() as Types.Mat3,
-                scalars.getData(),
-                volumeId
-              )
-            );
-          });
+        console.log('Url : ' + url);
+        console.log('Options : ');
+        // console.log(options);
+        let reader;
+        if (xml) reader = vtkXMLImageDataReader.newInstance();
+        else
+          reader = vtkHttpDataSetReader.newInstance({ fetchGzip: fetchGzip });
+
+        reader.setUrl(url).then(() => {
+          const data = reader.getOutputData();
+          const scalars = reader.getOutputData().getPointData().getScalars();
+          console.log(data.getDimensions());
+          console.log(scalars);
+          resolve(
+            createImageVolume(
+              scalars.getDataType() as string,
+              data.getDimensions() as Types.Point3,
+              data.getSpacing() as Types.Point3,
+              data.getOrigin() as Types.Point3,
+              data.getDirection() as Types.Mat3,
+              scalars.getData(),
+              volumeId
+            )
+          );
+        });
       } catch (error: unknown) {
         reject(error);
       }
