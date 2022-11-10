89052baed7a9ffefa1abbaf370ae5f0239fe3a00
Neil <neil.a.macphee@gmail.com>
2022-11-10 14:37:34 -0500

    vtpVolumeLoader example with 3D viewport

---
+++ packages/tools/examples/vtp3dViewport/index.ts
@@ -0,0 +1,227 @@
+import {
+  RenderingEngine,
+  Types,
+  Enums,
+  setVolumesForViewports,
+  volumeLoader,
+} from '@cornerstonejs/core';
+import {
+  initDemo,
+  createImageIdsAndCacheMetaData,
+  setTitleAndDescription,
+} from '../../../../utils/demo/helpers';
+import * as cornerstoneTools from '@cornerstonejs/tools';
+import applyPreset from './applyPreset';
+import presets from './presets';
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
+import vtkPolyDataReader from '@kitware/vtk.js/IO/Legacy/PolyDataReader';
+import vtkXMLPolyDataReader from '@kitware/vtk.js/IO/XML/XMLPolyDataReader';
+import vtkColorTransferFunction from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction';
+
+// import { vtiVolumeLoader } from '../../../core/examples/vtiVolumeLoader/vtiVolumeLoader';
+
+// This is for debugging purposes
+console.warn(
+  'Click on index.ts to open source code for this example --------->'
+);
+
+const {
+  LengthTool,
+  ToolGroupManager,
+  TrackballRotateTool,
+  StackScrollMouseWheelTool,
+  ZoomTool,
+  Enums: csToolsEnums,
+} = cornerstoneTools;
+
+const { ViewportType } = Enums;
+const { MouseBindings } = csToolsEnums;
+
+// Define a unique id for the volume
+const volumeName = 'CT_VOLUME_ID'; // Id of the volume less loader prefix
+const volumeLoaderScheme = 'vtiVolumeLoader'; // Loader id which defines which volume loader to use
+const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
+
+// ======== Set up page ======== //
+setTitleAndDescription(
+  'Annotation Tools On Volumes',
+  'Here we demonstrate how annotation tools can be drawn/rendered on any plane.'
+);
+
+const size = '500px';
+const content = document.getElementById('content');
+const viewportGrid = document.createElement('div');
+
+viewportGrid.style.display = 'flex';
+viewportGrid.style.display = 'flex';
+viewportGrid.style.flexDirection = 'row';
+
+const element1 = document.createElement('div');
+
+element1.oncontextmenu = () => false;
+
+element1.style.width = size;
+element1.style.height = size;
+
+viewportGrid.appendChild(element1);
+
+content.appendChild(viewportGrid);
+
+const instructions = document.createElement('p');
+instructions.innerText =
+  'Left Click to draw length measurements on any viewport.\n Use the mouse wheel to scroll through the stack.';
+
+content.append(instructions);
+
+async function getXML(url, viewport) {
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  const vtpReader = vtkXMLPolyDataReader.newInstance();
+  vtpReader.parseAsArrayBuffer(arrayBuffer);
+
+  const lookupTable = vtkColorTransferFunction.newInstance();
+  const source = vtpReader.getOutputData(0);
+  const mapper = vtkMapper.newInstance({
+    interpolateScalarsBeforeMapping: false,
+    useLookupTableScalarRange: true,
+    lookupTable,
+    scalarVisibility: false,
+  });
+  const actor = vtkActor.newInstance();
+  console.log(`Actor info:`);
+  console.log(actor.getClassName());
+  actor.setMapper(mapper);
+  mapper.setInputData(source);
+  viewport.setActors([{ uid: 'vtpActor', actor }]);
+  viewport.render();
+}
+// ============================= //
+
+/**
+ * Runs the demo
+ */
+async function run() {
+  // Init Cornerstone and related libraries
+  await initDemo();
+
+  const toolGroupId = 'STACK_TOOL_GROUP_ID';
+
+  // Register volume loader
+
+  // Add tools to Cornerstone3D
+  cornerstoneTools.addTool(LengthTool);
+  cornerstoneTools.addTool(ZoomTool);
+  cornerstoneTools.addTool(TrackballRotateTool);
+  cornerstoneTools.addTool(StackScrollMouseWheelTool);
+
+  // Define a tool group, which defines how mouse events map to tool commands for
+  // Any viewport using the group
+  const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+
+  // Add the tools to the tool group and specify which volume they are pointing at
+  toolGroup.addTool(LengthTool.toolName, { configuration: { volumeId } });
+  toolGroup.addTool(TrackballRotateTool.toolName, {
+    configuration: { volumeId },
+  });
+  toolGroup.addTool(ZoomTool.toolName, { configuration: { volumeId } });
+  toolGroup.addTool(StackScrollMouseWheelTool.toolName);
+
+  // Set the initial state of the tools, here we set one tool active on left click.
+  // This means left click will draw that tool.
+  toolGroup.setToolActive(TrackballRotateTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Primary, // Left Click
+      },
+    ],
+  });
+
+  toolGroup.setToolActive(ZoomTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Secondary, // Right Click
+      },
+    ],
+  });
+
+  // As the Stack Scroll mouse wheel is a tool using the `mouseWheelCallback`
+  // hook instead of mouse buttons, it does not need to assign any mouse button.
+  toolGroup.setToolActive(StackScrollMouseWheelTool.toolName);
+
+  // Get Cornerstone imageIds and fetch metadata into RAM
+  const url = 'https://rapid-event-demo.s3.amazonaws.com/vessels.vtp';
+
+  // Instantiate a rendering engine
+  const renderingEngineId = 'myRenderingEngine';
+  const renderingEngine = new RenderingEngine(renderingEngineId);
+
+  // Create the viewports
+  const viewportIds = [
+    'CT_AXIAL_STACK',
+    // 'CT_SAGITTAL_STACK',
+    // 'CT_OBLIQUE_STACK',
+  ];
+
+  const viewportInputArray = [
+    {
+      viewportId: viewportIds[0],
+      type: ViewportType.ORTHOGRAPHIC,
+      element: element1,
+      defaultOptions: {
+        orientation: Enums.OrientationAxis.AXIAL,
+        background: <Types.Point3>[0, 0, 0],
+      },
+    },
+    // {
+    //   viewportId: viewportIds[1],
+    //   type: ViewportType.ORTHOGRAPHIC,
+    //   element: element2,
+    //   defaultOptions: {
+    //     orientation: Enums.OrientationAxis.SAGITTAL,
+    //     background: <Types.Point3>[0.2, 0, 0.2],
+    //   },
+    // },
+    // {
+    //   viewportId: viewportIds[2],
+    //   type: ViewportType.ORTHOGRAPHIC,
+    //   element: element3,
+    //   defaultOptions: {
+    //     orientation: {
+    //       // Random oblique orientation
+    //       viewUp: <Types.Point3>[
+    //         -0.5962687530844388, 0.5453181550345819, -0.5891448751239446,
+    //       ],
+    //       viewPlaneNormal: <Types.Point3>[
+    //         -0.5962687530844388, 0.5453181550345819, -0.5891448751239446,
+    //       ],
+    //     },
+    //     background: <Types.Point3>[0.2, 0, 0.2],
+    //   },
+    // },
+  ];
+
+  renderingEngine.setViewports(viewportInputArray);
+
+  // Set the tool group on the viewports
+  viewportIds.forEach((viewportId) =>
+    toolGroup.addViewport(viewportId, 'myRenderingEngine')
+  );
+
+  // Render the image
+  renderingEngine.renderViewports(viewportIds);
+
+  console.log(renderingEngine);
+
+  const viewport = <Types.IVolumeViewport>(
+    renderingEngine.getViewport(viewportIds[0])
+  );
+
+  console.log(`Viewport`);
+  console.log(viewport);
+  // console.log(viewport);
+
+  getXML(url, viewport);
+}
+
+run();
