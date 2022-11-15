2829e4cdd5bd999156a6f1e063e932c56fec408c
Neil <neil.a.macphee@gmail.com>
2022-11-15 14:09:33 -0500

    Adding image IDs for demo

--- packages/tools/examples/vtp3dViewport/index.ts
+++ packages/tools/examples/vtp3dViewport/index.ts
@@ -150,6 +150,16 @@ async function run() {
   toolGroup.setToolActive(StackScrollMouseWheelTool.toolName);
 
   // Get Cornerstone imageIds and fetch metadata into RAM
+  const imageIds = await createImageIdsAndCacheMetaData({
+    StudyInstanceUID:
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
+    SeriesInstanceUID:
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
+    wadoRsRoot: 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb',
+    type: 'VOLUME',
+  });
+
+  // Get Cornerstone imageIds and fetch metadata into RAM
   const url = 'https://rapid-event-demo.s3.amazonaws.com/vessels.vtp';
 
   // Instantiate a rendering engine
